90aa0cbf38c747e7a5ac6d8d3ba1bfec1fb6c6dc
dima <wxllfwlr@gmail.com>
2022-02-04 18:44:02 +0300

    .

--- javascripts/java-script.js
+++ javascripts/java-script.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 });
 
 $(document).ready(function(){
-	$("div").click(function(){
+	$(".box1, .box4, .box5, .box6, .box7, .box8, .box9, .box10").click(function(){
 		$(this).toggleClass("black", 1000, "easeInBack");
 	});
 });
@@ -16,9 +16,9 @@ $(".box3").click(function(){
 
 $(document).ready(function(){
 $(".box2").hover(function(){
-  $(this).addClass("black");
+  $(this).addClass("black", 1800, "easeIn");
 }, function() {
-  $(this).removeClass("black");
+  $(this).removeClass("black", 1800, "easeBack");
     });
 });
 
@@ -37,5 +37,11 @@ $(document).ready(function(){
 $(document).ready(function(){
   $( ".biggie" ).click(function() {
     $( this ).removeClass( "big-blue", 1000, "easeInBack" );
+  });
+});
+
+$(document).ready(function(){
+$(".box11").click(function(){
+  $(this).hide(1200);
   });
 });
